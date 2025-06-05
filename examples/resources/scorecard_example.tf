e15513ba22fee412cbed8620f02419d7489e10e9
Andrew Jensen <andrewjensen90@gmail.com>
2025-06-05 16:20:21 -0600

    Add more example config

--- examples/resources/scorecard_example.tf
+++ examples/resources/scorecard_example.tf
@@ -18,7 +18,7 @@ resource "dx_scorecard" "example" {
   entity_filter_type             = "entity_types"
   entity_filter_type_identifiers = ["service"]
   evaluation_frequency_hours     = 2
-  empty_level_label              = "None"
+  empty_level_label              = "Incomplete"
   empty_level_color              = "#cccccc"
   published                      = true
 
@@ -45,20 +45,47 @@ resource "dx_scorecard" "example" {
   checks = [
     {
       name                  = "Test Check"
+      scorecard_level_key   = "bronze"
+      ordering              = 0
+
       description           = "This is a test check"
       sql                   = <<-EOT
-        select
-          'PASS' as status,
-          123 as output
+        select 'PASS' as status, 123 as output
       EOT
-      scorecard_level_key   = "bronze"
       output_enabled        = true
       output_type           = "duration_seconds"
       output_aggregation    = "median"
-      ordering              = 0
       external_url          = "http://example.com"
       published             = true
       estimated_dev_days    = 1.5
+      filter_message        = ""
+      filter_sql            = ""
+      output_custom_options = null
+    },
+    {
+      name                  = "Another Check"
+      scorecard_level_key   = "silver"
+      ordering              = 0
+
+      description           = "This is a another test check"
+      sql                   = <<-EOT
+        with random_number as (
+          select ROUND(RANDOM() * 10) as value
+        )
+        select case
+            when value >= 7 then 'PASS'
+            when value >= 4 then 'WARN'
+            else 'FAIL'
+          end as status,
+          value as output
+        from random_number
+      EOT
+      output_enabled        = true
+      output_type           = "duration_seconds"
+      output_aggregation    = "median"
+      external_url          = "http://example.com"
+      published             = false
+      estimated_dev_days    = null
       filter_message        = ""
       filter_sql            = ""
       output_custom_options = null
