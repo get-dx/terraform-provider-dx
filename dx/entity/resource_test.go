40fb28f90cf7c8d08f2d5259dea582d4d397ea80
Tyler Wray <wraytw@gmail.com>
2025-12-02 16:28:42 -0700

    Add test

---
+++ dx/entity/resource_test.go
@@ -0,0 +1,231 @@
+package entity_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"terraform-provider-dx/internal/acctest"
+)
+
+func TestAccDxEntityResourceCreate(t *testing.T) {
+	entityIdentifier := fmt.Sprintf("tf_test_entity_%d", acctest.RandInt())
+	entityName := fmt.Sprintf("Terraform Test Entity %d", acctest.RandInt())
+
+	var testAccDxEntityResourceBasic = fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "tf-integration-test" {
+  identifier  = "%s"
+  type        = "service"
+  name        = "%s"
+  description = "This is a test entity created by Terraform"
+
+  properties = {
+    tier            = "Tier-1"
+    "slack-channel" = "https://slack.com/channels/test-channel"
+    languages       = ["Go", "TypeScript"]
+  }
+
+  aliases = {
+    github_repo = [
+      {
+        identifier = "123456789"
+      }
+    ]
+  }
+
+  relations = {
+    "service-consumes-api" = ["users.search"]
+  }
+}
+`, entityIdentifier, entityName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccDxEntityResourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "identifier", entityIdentifier),
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "type", "service"),
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "name", entityName),
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "description", "This is a test entity created by Terraform"),
+					resource.TestCheckResourceAttrSet("dx_entity.tf-integration-test", "created_at"),
+					resource.TestCheckResourceAttrSet("dx_entity.tf-integration-test", "updated_at"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "dx_entity.tf-integration-test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read testing
+			{
+				Config: fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "tf-integration-test" {
+  identifier  = "%s"
+  type        = "service"
+  name        = "%s Updated"
+  description = "Updated description for test entity"
+
+  properties = {
+    tier            = "Tier-2"
+    "slack-channel" = "https://slack.com/channels/updated-channel"
+    languages       = ["Go", "TypeScript", "Python"]
+    active          = true
+  }
+
+  aliases = {
+    github_repo = [
+      {
+        identifier = "123456789"
+      },
+      {
+        identifier = "987654321"
+      }
+    ]
+  }
+
+  relations = {
+    "service-consumes-api" = ["users.search", "users.list"]
+  }
+}
+`, entityIdentifier, entityName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "name", entityName+" Updated"),
+					resource.TestCheckResourceAttr("dx_entity.tf-integration-test", "description", "Updated description for test entity"),
+				),
+			},
+			// Delete testing automatically occurs at the end
+		},
+	})
+}
+
+func TestAccDxEntityResourceMinimal(t *testing.T) {
+	entityIdentifier := fmt.Sprintf("tf_minimal_entity_%d", acctest.RandInt())
+
+	var testAccDxEntityResourceMinimal = fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "minimal" {
+  identifier = "%s"
+  type       = "service"
+}
+`, entityIdentifier)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDxEntityResourceMinimal,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.minimal", "identifier", entityIdentifier),
+					resource.TestCheckResourceAttr("dx_entity.minimal", "type", "service"),
+					resource.TestCheckResourceAttrSet("dx_entity.minimal", "created_at"),
+					resource.TestCheckResourceAttrSet("dx_entity.minimal", "updated_at"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDxEntityResourceWithOptionalFields(t *testing.T) {
+	entityIdentifier := fmt.Sprintf("tf_optional_entity_%d", acctest.RandInt())
+	entityName := fmt.Sprintf("Optional Fields Entity %d", acctest.RandInt())
+
+	// Test with some optional fields but not all (no aliases, no relations)
+	var testAccDxEntityResourceOptional = fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "optional" {
+  identifier  = "%s"
+  type        = "service"
+  name        = "%s"
+  description = "Entity with some optional fields"
+
+  properties = {
+    tier = "Tier-3"
+  }
+}
+`, entityIdentifier, entityName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDxEntityResourceOptional,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.optional", "identifier", entityIdentifier),
+					resource.TestCheckResourceAttr("dx_entity.optional", "type", "service"),
+					resource.TestCheckResourceAttr("dx_entity.optional", "name", entityName),
+					resource.TestCheckResourceAttr("dx_entity.optional", "description", "Entity with some optional fields"),
+					resource.TestCheckResourceAttrSet("dx_entity.optional", "created_at"),
+					resource.TestCheckResourceAttrSet("dx_entity.optional", "updated_at"),
+				),
+			},
+			// Update to add aliases and relations
+			{
+				Config: fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "optional" {
+  identifier  = "%s"
+  type        = "service"
+  name        = "%s"
+  description = "Entity with some optional fields"
+
+  properties = {
+    tier = "Tier-3"
+  }
+
+  aliases = {
+    github_repo = [
+      {
+        identifier = "555555555"
+      }
+    ]
+  }
+
+  relations = {
+    "service-consumes-api" = ["some-api"]
+  }
+}
+`, entityIdentifier, entityName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.optional", "identifier", entityIdentifier),
+					resource.TestCheckResourceAttr("dx_entity.optional", "type", "service"),
+				),
+			},
+			// Update to remove aliases and relations (back to null)
+			{
+				Config: fmt.Sprintf(`
+provider "dx" {}
+
+resource "dx_entity" "optional" {
+  identifier  = "%s"
+  type        = "service"
+  name        = "%s"
+  description = "Entity with some optional fields"
+
+  properties = {
+    tier = "Tier-3"
+  }
+}
+`, entityIdentifier, entityName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("dx_entity.optional", "identifier", entityIdentifier),
+					resource.TestCheckResourceAttr("dx_entity.optional", "type", "service"),
+				),
+			},
+		},
+	})
+}
